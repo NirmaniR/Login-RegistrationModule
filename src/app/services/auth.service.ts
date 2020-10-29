f4591b50673e8a7bbb676fedceabad52c6bb910c
nirmani <sadudnr@gmail.com>
2020-10-29 15:17:03 +0530

    first commit

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import{ AngularFireAuth} from '@angular/fire/auth';
+import {Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  authState:any=null;
+  
+
+  constructor(private afu: AngularFireAuth, private router:Router) { 
+    this.afu.authState.subscribe((auth =>{
+      this.authState = auth;
+    }))
+  }
+  get isUserAnonymousLoggedIn(): boolean {
+    return (this.authState !== null) ? this.authState.isAnonymous : false
+  }
+
+  get currentUserId(): string {
+    return (this.authState !== null) ? this.authState.uid : ''
+  }
+
+  get currentUserName(): string {
+    return this.authState['email']
+  }
+
+  get currentUser(): any {
+    return (this.authState !== null) ? this.authState : null;
+  }
+
+  get isUserEmailLoggedIn(): boolean {
+    if ((this.authState !== null) && (!this.isUserAnonymousLoggedIn)) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  async registerWithEmail(email:string , password:string){
+    try {
+      const user = await this.afu.createUserWithEmailAndPassword(email, password);
+      this.authState = user;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  async loginWithEmail(email: string, password: string)
+  {
+    try {
+      const user = await this.afu.signInWithEmailAndPassword(email, password);
+      this.authState = user;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  singout(): void
+  {
+    this.afu.signOut();
+    this.router.navigate(['/login']);
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase :{
+    apiKey: "AIzaSyCF5xSph726YS-CxMPFUFD4GsMoVI9g03k",
+    authDomain: "fir-auth-be993.firebaseapp.com",
+    databaseURL: "https://fir-auth-be993.firebaseio.com",
+    projectId: "fir-auth-be993",
+    storageBucket: "fir-auth-be993.appspot.com",
+    messagingSenderId: "349181316425",
+    appId: "1:349181316425:web:2314d5e694fba627675e98"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
